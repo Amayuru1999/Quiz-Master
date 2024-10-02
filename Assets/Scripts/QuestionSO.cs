1bfbd15a3a9a547fd416ba48ca16d0016771460c
Amayuru1999 <amarasingheau@gmail.com>
2024-10-02 14:58:36 +0530

    feat: Added arrays to the game.

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -7,4 +7,11 @@ public class QuestionSO : ScriptableObject
 {
   [TextArea(2,6)]
   [SerializeField] string question = "Enter new question here";
+  string[] answers = new string[4];
+  
+  public string GetQuestion()
+  {
+    return question;
+  }
 }
+
