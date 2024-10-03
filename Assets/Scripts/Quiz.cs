96fef587fad1adb59fca5053e2818746ca759ee3
Amayuru1999 <amarasingheau@gmail.com>
2024-10-03 10:51:10 +0530

    feat: Added Display Question method in Quiz.cs #1

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -51,5 +51,14 @@ public class Quiz : MonoBehaviour
             buttonText.text = question.GetAnswer(i);
         }
     }
+
+    void SetButtonState(bool state)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
     
 }
